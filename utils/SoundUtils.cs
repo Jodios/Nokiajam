2bf4324bc4408a5e9c508e6a4a92dc8718a225f5
kasarova <kasarova00@gmail.com>
2024-02-20 08:24:34 -0800

    added basic sound for shooting

--- utils/SoundUtils.cs
+++ utils/SoundUtils.cs
@@ -7,9 +7,7 @@ public partial class SoundUtils : Node
 	{
 		float left = AudioServer.GetBusPeakVolumeLeftDb(0, 0);
 		float right = AudioServer.GetBusPeakVolumeLeftDb(0, 0);
-		GD.Print(left + "     :      " + right);
 		if (left > -199 || right > -199 ) return;
-		GD.Print("Playing");
 		player.Play(0);
 	}
 }

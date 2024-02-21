313c84fe33b28e4a85c42417e97c8ff8b65437c9
kasarova <kasarova00@gmail.com>
2024-02-20 19:13:07 -0800

    Adding sound

--- utils/SoundUtils.cs
+++ utils/SoundUtils.cs
@@ -1,13 +1,37 @@
 using Godot;
-using System;
 
+/*
+Started regretting doing it this way mid way :D 
+*/
 public partial class SoundUtils : Node
 {
-	public static void PlaySound(AudioStreamPlayer player)
+	public AudioStreamPlayer HealthItem;
+	public AudioStreamPlayer FreezeItem;
+	public AudioStreamPlayer Freeze;
+	public AudioStreamPlayer Perish;
+	public AudioStreamPlayer EnemyHit;
+	public AudioStreamPlayer Shoot;
+	private bool playing = false;
+	public override void _Ready()
 	{
-		float left = AudioServer.GetBusPeakVolumeLeftDb(0, 0);
-		float right = AudioServer.GetBusPeakVolumeLeftDb(0, 0);
-		if (left > -199 || right > -199 ) return;
+		HealthItem = GetNode<AudioStreamPlayer>("HealthItem");
+		FreezeItem = GetNode<AudioStreamPlayer>("FreezeItem");
+		Freeze = GetNode<AudioStreamPlayer>("Freeze");
+		Perish = GetNode<AudioStreamPlayer>("Perish");
+		EnemyHit = GetNode<AudioStreamPlayer>("EnemyHit");
+		Shoot = GetNode<AudioStreamPlayer>("Shoot");
+	}
+	public void PlayPickupHealthItemSound() { PlaySound(HealthItem); }
+	public void PlayPickupFreezeItemSound() { PlaySound(FreezeItem); }
+	public void PlayFreezeActionSound() { PlaySound(Freeze); }
+	public void PlayPerishSound() { PlaySound(Perish); }
+	public void PlayEnemyHitSound() { PlaySound(EnemyHit); }
+	public void PlayShootingSound() { PlaySound(Shoot); }
+	private void PlaySound(AudioStreamPlayer player)
+	{
+		if (playing) return;
+		playing = true;
 		player.Play(0);
+		player.Finished += () => { playing = false; };
 	}
 }

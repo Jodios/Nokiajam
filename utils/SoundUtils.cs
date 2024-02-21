bc434070ec755d43e2eb9ce781d60f06bb005653
kasarova <kasarova00@gmail.com>
2024-02-20 19:46:30 -0800

    added sound :)

--- utils/SoundUtils.cs
+++ utils/SoundUtils.cs
@@ -15,11 +15,22 @@ public partial class SoundUtils : Node
 	public override void _Ready()
 	{
 		HealthItem = GetNode<AudioStreamPlayer>("HealthItem");
+		HealthItem.Finished += Finished;
+
 		FreezeItem = GetNode<AudioStreamPlayer>("FreezeItem");
+		FreezeItem.Finished += Finished;
+
 		Freeze = GetNode<AudioStreamPlayer>("Freeze");
+		Freeze.Finished += Finished;
+
 		Perish = GetNode<AudioStreamPlayer>("Perish");
+		Perish.Finished += Finished;
+
 		EnemyHit = GetNode<AudioStreamPlayer>("EnemyHit");
+		EnemyHit.Finished += Finished;
+
 		Shoot = GetNode<AudioStreamPlayer>("Shoot");
+		Shoot.Finished += Finished;
 	}
 	public void PlayPickupHealthItemSound() { PlaySound(HealthItem); }
 	public void PlayPickupFreezeItemSound() { PlaySound(FreezeItem); }
@@ -32,6 +43,9 @@ public partial class SoundUtils : Node
 		if (playing) return;
 		playing = true;
 		player.Play(0);
-		player.Finished += () => { playing = false; };
+	}
+
+	private void Finished() {
+		playing = false;
 	}
 }

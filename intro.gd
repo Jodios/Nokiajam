f6db1839e360ddd052e10a05c8eeed11a07bf58b
kasarova <kasarova00@gmail.com>
2024-02-25 10:04:20 -0800

    Updating menu skip and tweaking times

--- intro.gd
+++ intro.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var animationPlayer: AnimationPlayer = $AnimationPlayer
 @onready var background = $background
+var startable: bool = false
 
 func _ready():
 	SoundUtils.play_music()
@@ -9,9 +10,14 @@ func _ready():
 	animationPlayer.play("introPart1")
 	animationPlayer.animation_finished.connect(func(_name):
 		animationPlayer.play("introPart2")
+		startable = true
 	)
 
 func _process(_delta):
 	if Input.is_action_just_pressed("shoot"):
-		SoundUtils.stop_music()
-		get_tree().change_scene_to_file("res://levels/Main.tscn")
+		if !startable:
+			animationPlayer.play("introPart2")
+			startable = true
+		else:
+			SoundUtils.stop_music()
+			get_tree().change_scene_to_file("res://levels/Main.tscn")
